0404bc1ece69d8165173c58ea073b7344aba7a47
Robert LeBourdais <rlebourd@bu.edu>
2021-11-17 13:47:07 -0500

    Split imageIOgzType.h into two files (h and cpp) and added header guards

---
+++ src/imageIOgzType.cpp
@@ -0,0 +1,157 @@
+#include "imageIOgzType.h"
+
+void writeNifti(string filestr,float* vol,char* header,int m,int n,int o,int k){
+    //convert input string into char* array
+    char* filename=new char[filestr.size()+1];
+    copy(filestr.begin(),filestr.end(),filename);
+    filename[filestr.size()]='\0';
+    
+    char* header2=new char[352];
+    copy(header,header+352,header2);
+    int dim=o>1?3:2;
+    if(k>1){
+        dim=4;
+    }
+    short* dimensions=new short[4];
+    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n; dimensions[3]=o;
+    dimensions[4]=k;
+    char* dimchar=reinterpret_cast<char*>(dimensions);
+    copy(dimchar,dimchar+8,header2+40);
+
+    short* datatype=new short[2];
+    datatype[0]=16; datatype[1]=32; //float datatype
+    char* datachar=reinterpret_cast<char*>(datatype);
+    copy(datachar,datachar+4,header2+70);
+    
+    //printf("Writing image with dimensions %dx%dx%d\n",m,n,o);
+
+    ofstream file(filename,ios::out|ios::binary);
+    //opens file for binary-output
+	if(file.is_open()){
+		file.write(header2,352);
+		file.write(reinterpret_cast<char*>(vol),m*n*o*4);
+		file.close();
+		cout<<"File "<<filename<<" written.\n";
+	}
+    else{
+        printf("File error. Could not write file.\n");
+    }
+}
+
+void writeSegment(string filestr,short* vol,char* header,int m,int n,int o){
+    //convert input string into char* array
+    char* filename=new char[filestr.size()+1];
+    copy(filestr.begin(),filestr.end(),filename);
+    filename[filestr.size()]='\0';
+    
+    char* header2=new char[352];
+    copy(header,header+352,header2);
+    int dim=o>1?3:2;
+    short* dimensions=new short[4];
+    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n; dimensions[3]=o;
+    char* dimchar=reinterpret_cast<char*>(dimensions);
+    copy(dimchar,dimchar+8,header2+40);
+    
+    short* datatype=new short[2];
+    datatype[0]=4; datatype[1]=512; //short datatype
+    char* datachar=reinterpret_cast<char*>(datatype);
+    copy(datachar,datachar+4,header2+70);
+    
+    //printf("Writing segmentation with dimensions %dx%dx%d\n",m,n,o);
+    
+    ofstream file(filename,ios::out|ios::binary);
+    //opens file for binary-output
+	if(file.is_open()){
+		file.write(header2,352);
+		file.write(reinterpret_cast<char*>(vol),m*n*o*2);
+		file.close();
+		cout<<"File "<<filename<<" written.\n";
+	}
+    else{
+        printf("File error. Could not write file.\n");
+    }
+}
+
+
+void gzWriteNifti(string filestr,float* data,char* header,int m,int n,int o,int p){
+    
+    int sz=m*n*o*p;
+    //convert input string into char* array
+    char* filename=new char[filestr.size()+1];
+    copy(filestr.begin(),filestr.end(),filename);
+    filename[filestr.size()]='\0';
+    
+    //do not change original header, define (new )dimensions and datatype
+    char* header2=new char[352];
+    copy(header,header+352,header2);
+    int dim=o>1?3:2;
+    if(p>1){ dim=4;}
+    short* dimensions=new short[4];
+    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n;
+    dimensions[3]=o; dimensions[4]=p;
+    char* dimchar=reinterpret_cast<char*>(dimensions);
+    copy(dimchar,dimchar+10,header2+40);
+    short* datatype=new short[2];
+    datatype[0]=16; datatype[1]=32; //float datatype
+    char* typechar=reinterpret_cast<char*>(datatype);
+    copy(typechar,typechar+4,header2+70);
+    
+    //copy header and data into out
+    int size=352+sz*sizeof(float);
+    char* out2=new char[size];
+    copy(header2,header2+352,out2);
+    char* datachar=reinterpret_cast<char*>(data);
+    copy(datachar,datachar+sz*sizeof(float),out2+352);
+    
+    //write gzipped file
+    gzFile file2=gzopen(filename,"wb");
+    if(!file2){
+        printf("File error. Could not write file.\n");
+        exit(1);
+    }
+    int err=gzwrite(file2,(unsigned char*)out2,size);
+    gzclose(file2);
+    
+    delete out2; delete header2;
+}
+
+void gzWriteSegment(string filestr,short* data,char* header,int m,int n,int o,int p){
+    int sz=m*n*o*p;
+    //convert input string into char* array
+    char* filename=new char[filestr.size()+1];
+    copy(filestr.begin(),filestr.end(),filename);
+    filename[filestr.size()]='\0';
+    
+    //do not change original header, define (new )dimensions and datatype
+    char* header2=new char[352];
+    copy(header,header+352,header2);
+    int dim=o>1?3:2;
+    if(p>1){ dim=4;}
+    short* dimensions=new short[4];
+    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n;
+    dimensions[3]=o; dimensions[4]=p;
+    char* dimchar=reinterpret_cast<char*>(dimensions);
+    copy(dimchar,dimchar+10,header2+40);
+    short* datatype=new short[2];
+    datatype[0]=4; datatype[1]=16;// edit 08-16 - 512; //short datatype
+    char* typechar=reinterpret_cast<char*>(datatype);
+    copy(typechar,typechar+4,header2+70);
+    
+    //copy header and data into out
+    int size=352+sz*sizeof(short);
+    char* out2=new char[size];
+    copy(header2,header2+352,out2);
+    char* datachar=reinterpret_cast<char*>(data);
+    copy(datachar,datachar+sz*sizeof(short),out2+352);
+    
+    //write gzipped file
+    gzFile file2=gzopen(filename,"wb");
+    if(!file2){
+        printf("File error. Could not write file.\n");
+        exit(1);
+    }
+    int err=gzwrite(file2,(unsigned char*)out2,size);
+    gzclose(file2);
+    
+    delete out2; delete header2;
+}
--- src/imageIOgzType.h
+++ src/imageIOgzType.h
@@ -1,9 +1,20 @@
 /*
- Mattias P. Heinrich
- Universitaet Luebeck, 2014
+  Adapted from Mattias P. Heinrich
+  Universitaet Luebeck, 2014
  */
-template <typename Type>
 
+#ifndef IMAGE_IOGZ_TYPE_H_INCLUDED
+#define IMAGE_IOGZ_TYPE_H_INCLUDED
+
+void writeNifti(string filestr,float* vol,char* header,int m,int n,int o,int k);
+
+void writeSegment(string filestr,short* vol,char* header,int m,int n,int o);
+
+void gzWriteNifti(string filestr,float* data,char* header,int m,int n,int o,int p);
+
+void gzWriteSegment(string filestr,short* data,char* header,int m,int n,int o,int p);
+
+template <typename Type>
 void readNifti(string filestr,Type*& vol,char*& header,int& m,int& n,int& o,int& p){
     
     //convert input string into char* array
@@ -104,166 +115,6 @@ void readNifti(string filestr,Type*& vol,char*& header,int& m,int& n,int& o,int&
     
 }
 
-
-void writeNifti(string filestr,float* vol,char* header,int m,int n,int o,int k){
-    //convert input string into char* array
-    char* filename=new char[filestr.size()+1];
-    copy(filestr.begin(),filestr.end(),filename);
-    filename[filestr.size()]='\0';
-    
-    char* header2=new char[352];
-    copy(header,header+352,header2);
-    int dim=o>1?3:2;
-    if(k>1){
-        dim=4;
-    }
-    short* dimensions=new short[4];
-    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n; dimensions[3]=o;
-    dimensions[4]=k;
-    char* dimchar=reinterpret_cast<char*>(dimensions);
-    copy(dimchar,dimchar+8,header2+40);
-
-    short* datatype=new short[2];
-    datatype[0]=16; datatype[1]=32; //float datatype
-    char* datachar=reinterpret_cast<char*>(datatype);
-    copy(datachar,datachar+4,header2+70);
-    
-    //printf("Writing image with dimensions %dx%dx%d\n",m,n,o);
-
-    ofstream file(filename,ios::out|ios::binary);
-    //opens file for binary-output
-	if(file.is_open()){
-		file.write(header2,352);
-		file.write(reinterpret_cast<char*>(vol),m*n*o*4);
-		file.close();
-		cout<<"File "<<filename<<" written.\n";
-	}
-    else{
-        printf("File error. Could not write file.\n");
-    }
-}
-
-void writeSegment(string filestr,short* vol,char* header,int m,int n,int o){
-    //convert input string into char* array
-    char* filename=new char[filestr.size()+1];
-    copy(filestr.begin(),filestr.end(),filename);
-    filename[filestr.size()]='\0';
-    
-    char* header2=new char[352];
-    copy(header,header+352,header2);
-    int dim=o>1?3:2;
-    short* dimensions=new short[4];
-    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n; dimensions[3]=o;
-    char* dimchar=reinterpret_cast<char*>(dimensions);
-    copy(dimchar,dimchar+8,header2+40);
-    
-    short* datatype=new short[2];
-    datatype[0]=4; datatype[1]=512; //short datatype
-    char* datachar=reinterpret_cast<char*>(datatype);
-    copy(datachar,datachar+4,header2+70);
-    
-    //printf("Writing segmentation with dimensions %dx%dx%d\n",m,n,o);
-    
-    ofstream file(filename,ios::out|ios::binary);
-    //opens file for binary-output
-	if(file.is_open()){
-		file.write(header2,352);
-		file.write(reinterpret_cast<char*>(vol),m*n*o*2);
-		file.close();
-		cout<<"File "<<filename<<" written.\n";
-	}
-    else{
-        printf("File error. Could not write file.\n");
-    }
-}
-
-
-void gzWriteNifti(string filestr,float* data,char* header,int m,int n,int o,int p){
-    
-    int sz=m*n*o*p;
-    //convert input string into char* array
-    char* filename=new char[filestr.size()+1];
-    copy(filestr.begin(),filestr.end(),filename);
-    filename[filestr.size()]='\0';
-    
-    //do not change original header, define (new )dimensions and datatype
-    char* header2=new char[352];
-    copy(header,header+352,header2);
-    int dim=o>1?3:2;
-    if(p>1){ dim=4;}
-    short* dimensions=new short[4];
-    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n;
-    dimensions[3]=o; dimensions[4]=p;
-    char* dimchar=reinterpret_cast<char*>(dimensions);
-    copy(dimchar,dimchar+10,header2+40);
-    short* datatype=new short[2];
-    datatype[0]=16; datatype[1]=32; //float datatype
-    char* typechar=reinterpret_cast<char*>(datatype);
-    copy(typechar,typechar+4,header2+70);
-    
-    //copy header and data into out
-    int size=352+sz*sizeof(float);
-    char* out2=new char[size];
-    copy(header2,header2+352,out2);
-    char* datachar=reinterpret_cast<char*>(data);
-    copy(datachar,datachar+sz*sizeof(float),out2+352);
-    
-    //write gzipped file
-    gzFile file2=gzopen(filename,"wb");
-    if(!file2){
-        printf("File error. Could not write file.\n");
-        exit(1);
-    }
-    int err=gzwrite(file2,(unsigned char*)out2,size);
-    gzclose(file2);
-    
-    delete out2; delete header2;
-}
-
-void gzWriteSegment(string filestr,short* data,char* header,int m,int n,int o,int p){
-    int sz=m*n*o*p;
-    //convert input string into char* array
-    char* filename=new char[filestr.size()+1];
-    copy(filestr.begin(),filestr.end(),filename);
-    filename[filestr.size()]='\0';
-    
-    //do not change original header, define (new )dimensions and datatype
-    char* header2=new char[352];
-    copy(header,header+352,header2);
-    int dim=o>1?3:2;
-    if(p>1){ dim=4;}
-    short* dimensions=new short[4];
-    dimensions[0]=dim; dimensions[1]=m; dimensions[2]=n;
-    dimensions[3]=o; dimensions[4]=p;
-    char* dimchar=reinterpret_cast<char*>(dimensions);
-    copy(dimchar,dimchar+10,header2+40);
-    short* datatype=new short[2];
-    datatype[0]=4; datatype[1]=16;// edit 08-16 - 512; //short datatype
-    char* typechar=reinterpret_cast<char*>(datatype);
-    copy(typechar,typechar+4,header2+70);
-    
-    //copy header and data into out
-    int size=352+sz*sizeof(short);
-    char* out2=new char[size];
-    copy(header2,header2+352,out2);
-    char* datachar=reinterpret_cast<char*>(data);
-    copy(datachar,datachar+sz*sizeof(short),out2+352);
-    
-    //write gzipped file
-    gzFile file2=gzopen(filename,"wb");
-    if(!file2){
-        printf("File error. Could not write file.\n");
-        exit(1);
-    }
-    int err=gzwrite(file2,(unsigned char*)out2,size);
-    gzclose(file2);
-    
-    delete out2; delete header2;
-    
-}
-
-
-
 template <typename TypeFO>
 void writeOutput(TypeFO* data,string filestr,int length){
     //opens file for binary-output
@@ -302,3 +153,5 @@ vector<TypeF> readFile(string filestr){
     file.close();
     return invalues;
 }
+
+#endif // IMAGE_IOGZ_TYPE_H_INCLUDED
