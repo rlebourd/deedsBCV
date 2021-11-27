9e90204cc612926461e681802a4cc6d93e812815
Robert LeBourdais <rlebourd@bu.edu>
2021-11-27 15:07:06 -0500

    Comments, nonfunctional changes, renaming

--- src/MINDSSCbox.cpp
+++ src/MINDSSCbox.cpp
@@ -84,13 +84,13 @@ void boxfilter(float* input, int hw, int m, int n, int o){
     for(int k = 0; k < o; k++){
         for(int i=0; i < m; i++){
             for(int j = 0; j < (hw + 1); j++){
-                temp1[i + j*m + k*m*n] = temp2[i + (j+hw)*m + k*m*n];
+                temp1[ind(i, j, k)] = temp2[ind(i, j+hw, k)];
             }
             for(int j = (hw + 1); j < (n - hw); j++){
-                temp1[i + j*m + k*m*n] = temp2[i + (j+hw)*m + k*m*n] - temp2[i + (j-hw-1)*m + k*m*n];
+                temp1[ind(i, j, k)] = temp2[ind(i, j+hw, k)] - temp2[ind(i, j-hw-1, k)];
             }
             for(int j = (n - hw); j < n; j++){
-                temp1[i + j*m + k*m*n] = temp2[i + (n-1)*m + k*m*n] - temp2[i + (j-hw-1)*m + k*m*n];
+                temp1[ind(i, j, k)] = temp2[ind(i, n-1, k)] - temp2[ind(i, j-hw-1, k)];
             }
         }
     }
@@ -99,7 +99,7 @@ void boxfilter(float* input, int hw, int m, int n, int o){
     for(int k = 1; k < o; k++){
         for(int j = 0; j < n; j++){
             for(int i = 0; i < m; i++){
-                temp1[i + j*m + k*m*n] += temp1[i + j*m + (k-1)*m*n];
+                temp1[ind(i, j, k)] += temp1[ind(i, j, k-1)];
             }
         }
     }
@@ -108,13 +108,13 @@ void boxfilter(float* input, int hw, int m, int n, int o){
     for(int j = 0; j < n; j++){
         for(int i = 0; i < m; i++){
             for(int k = 0; k < (hw+1); k++){
-                input[i + j*m + k*m*n] = temp1[i+j*m+(k+hw)*m*n];
+                input[ind(i, j, k)] = temp1[ind(i, j, k+hw)];
             }
             for(int k = (hw+1); k < (o-hw); k++){
-                input[i + j*m + k*m*n] = temp1[i+j*m+(k+hw)*m*n]-temp1[i+j*m+(k-hw-1)*m*n];
+                input[ind(i, j, k)] = temp1[ind(i, j, k+hw)]-temp1[ind(i, j, k-hw-1)];
             }
             for(int k = (o-hw); k < o; k++){
-                input[i + j*m + k*m*n] = temp1[i+j*m+(o-1)*m*n]-temp1[i+j*m+(k-hw-1)*m*n];
+                input[ind(i, j, k)] = temp1[ind(i, j, o-1)]-temp1[ind(i, j, k-hw-1)];
             }
         }
     }
@@ -208,6 +208,13 @@ void descriptor(uint64_t* mindq, float* im1, int m, int n, int o, int qs){
     // The size p of the patch is determined by the parameter qs, which is calculated
     // as floor(quantisation/2 + 1) and which is called the "mind step" elsewhere in
     // the program.
+    //
+    // When we say that that "patch size is p," what we mean is that the side length
+    // of the patch is 2p+1. That is, there are p voxels directly to the left and p
+    // voxels directly to the right of the voxel at the center of the patch, making
+    // the side length equal to 2p+1. Similar statements can be made for any of the
+    // three coordinate axes.
+    //
     float *d1 = nullptr;
     distances(im1, &d1, m, n, o, qs);
     
@@ -249,19 +256,77 @@ void descriptor(uint64_t* mindq, float* im1, int m, int n, int o, int qs){
     const int numberOfShifts = 12;
     image_d = 12;
     
-    //quantisation table
-    const int val = 6;
-    
-#pragma omp parallel for
-    float mindThreshold[val-1];
-    for(int i = 0; i < val-1; i++){
-        mindThreshold[i] = -log((i+1.5f)/val);
+    // Quantisation table
+    //
+    // The original paper on MIND states that,
+    //
+    //   "In order to speed-up these computations
+    //    the descriptor can be quantised to only
+    //    4 bit, without significant loss of accuracy.
+    //
+    //    For |R| = 6 all possible distances between
+    //    descriptors can be pre-computed and stored
+    //    in a lookup-table."
+    //
+    // A 4 bit number can represent the numbers 0 to 15
+    // in decimal, 0 to 0xF in hexadecimal, and 0 to 1111
+    // in binary. So it can represent 16 different numbers.
+    //
+    // If |R| = 6, then the MIND descriptor has six 4-bit
+    // components, so each component can take on one of
+    // 16 different values. There are 16^6 different
+    // descriptors having six 4-bit components. This is
+    // 16.7 million possible descriptors, with each
+    // descriptor occupying 24 bits (3 bytes) of memory.
+    // Therefore, a table of all such descriptors would
+    // take up about 50 million bytes of memory, or 50 MB,
+    // which is a modest amount of memory even on a
+    // smartphone.
+    //
+    // Since there are 16.7 million different possible
+    // descriptors, we cannot store a lookup table
+    // that contains the distance between any two of
+    // these 16.7 million descriptors, because that would
+    // require about 280 trillion pairs of descriptors,
+    // which would far exceed the memory capacity of any
+    // modern computer.
+    //
+    // Perhaps when he says that the descriptor can be
+    // quantised to 4 bit, he means the entire descriptor
+    // and not just a single component. In this interpretation,
+    // it would be possible to store all possible distances
+    // between descriptors, because there would only be 256
+    // different pairs of descriptors. But that doesn't
+    // seem to make sense as an interpretation because the
+    // datatype for a descriptor is uint64_t.
+    //
+    // Perhaps what he means is that the distance is the
+    // distance between individual MIND *components* not
+    // between MIND *vectors*. This would reconcile all
+    // the available facts, in that the whole descriptor
+    // would require 24 bits of RAM, meaning that the
+    // uint64_t datatype would make some more sense as a
+    // choice, and that the distance between any pair of
+    // corresponding MIND *components* could be pre-
+    // computed and stored in a lookup table, specifically
+    // because there are 256 such possible pairs.
+    //
+    // With that said, I don't see a variable that stands
+    // out as a lookup table for precomputed distances,
+    // despite the fact that the original code was labeled
+    // as "Quantisation table."
+    //
+    const int numberOfMindThresholds = 6;
+    float mindThreshold[numberOfMindThresholds];
+    for(int i = 0; i < numberOfMindThresholds; i++){
+        mindThreshold[i] = -log((i+1.5f)/(numberOfMindThresholds+1));
     }
     
+#pragma omp parallel for
     for(int k = 0; k < o; k++){
         for(int j = 0; j < n; j++){
             for(int i = 0; i < m; i++){
-                float mindComponentsForCurrVoxel[12];
+                float mindComponentsForCurrVoxel[numberOfShifts];
 
                 // initialize the mind descriptors from the blurred distances
                 for(int shiftIndex = 0; shiftIndex < numberOfShifts; shiftIndex++){
@@ -315,6 +380,15 @@ void descriptor(uint64_t* mindq, float* im1, int m, int n, int o, int qs){
                 }
                 
                 //
+                // A reading of the software below seems to reveal that the MIND
+                // descriptor has *twelve*, and NOT six, 4-bit components.
+                //
+                // This can be seen by observing that, since numberOfShifts = 12,
+                // tabled1, which is initially equal to 1, is multiplied by 32
+                // twelve times. But (2^5)^12 = 2^60, which means that the
+                // the MIND descriptor requires 60 bits of memory in this
+                // representation. This explains why it is stored as a 64-bit
+                // value.
                 unsigned long long accum=0;
                 unsigned long long tabled1=1;
                 
@@ -323,7 +397,7 @@ void descriptor(uint64_t* mindq, float* im1, int m, int n, int o, int qs){
                     const unsigned int tablei[6] = {0, 1, 3, 7, 15, 31};
 
                     int mind1val = 0;
-                    for(int threshIndex = 0; threshIndex < val-1; threshIndex++){
+                    for(int threshIndex = 0; threshIndex < numberOfMindThresholds; threshIndex++){
                         mind1val += (mindThreshold[threshIndex] > mindComponentsForCurrVoxel[shiftIndex]) ? 1 : 0;
                     }
                     accum += tablei[mind1val] * tabled1;
