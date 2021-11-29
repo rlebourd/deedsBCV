9a72c31fe69810f247df163be8887e905dae06bc
Robert LeBourdais <rlebourd@bu.edu>
2021-11-29 10:50:00 -0500

    Updates to projects

---
+++ src/xcode-projects/linearBCV/linearBCV/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  linearBCV
+//
+//  Created by Robert LeBourdais on 11/17/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
