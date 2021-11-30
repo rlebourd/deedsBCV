914ccc1ab4b6ce88c941d1b08ebc25c8e2ef4a47
Robert LeBourdais <rlebourd@bu.edu>
2021-11-29 21:56:24 -0500

    Added some design thoughts

--- src/xcode-projects/descriptors/descriptors/main.cpp
+++ src/xcode-projects/descriptors/descriptors/main.cpp
@@ -47,8 +47,44 @@ static std::map<mind::IndexVector, mind::Matrix> createDistances(){
             // the patch vector could put the index below
             // the bounds of the matrix (i.e with negative
             // components)
-            const auto F1 = integratedDifferences.at(idx.subtracting(patchVector));
-            const auto F2 = integratedDifferences.at(idx.adding(patchVector));
+            //
+            // Identify how this error can be handled. One option
+            // is to handle the overflow or underflow by defaulting
+            // to a reasonable value, but arguably that hides a non-
+            // obvious implementation detail from explicit identification.
+            //
+            // It seems better to me to have the overflow or underflow
+            // reported by an exception. If the overflow or underflow
+            // occurs, then we default to a reasonable value explicitly.
+            //
+            // Another alternative is to design the API provided by the
+            // matrix class so that it is not possible to produce an
+            // exception, but where underflow and overflow are treated
+            // explicitly. The API would be something like:
+            //
+            // idx.subtractingWithDefaultValueOnUnderflow(patchVector, idx);
+            // idx.addingWithDefaultValueOnOverflow(patchVector, idx);
+            //
+            // This models the cases of underflow and overflow explicitly,
+            // drawing attention to them as important algorithmic details
+            // while providing an expressive API.
+            //
+            auto F1 = integratedDifferences.at(idx);
+            try {
+                F1 = integratedDifferences.at(idx.subtracting(patchVector));
+            } catch (...){
+                // do nothing in case of underflow
+            }
+
+            auto F2 = integratedDifferences.at(idx);
+            try {
+                F2 = integratedDifferences.at(idx.adding(patchVector));
+            } catch (...){
+                // do nothing in case of overflow
+            }
+            
+            //const auto F1 = integratedDifferences.at(idx.subtracting(patchVector));
+            //const auto F2 = integratedDifferences.at(idx.adding(patchVector));
             return (F2 - F1);
         };
         distances[dr] = integratedDifferences.mappingOverElements(convolution);
